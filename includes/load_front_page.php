f29c8bc7f57ef237affcc4c3043258541fa0e26a
Maxime Briand <contact@maximebriand.fr>
2019-04-23 17:15:03 +0200

    manage the url rewritting

--- includes/load_front_page.php
+++ includes/load_front_page.php
@@ -12,22 +12,70 @@ class Front_page {
     private $slug;
     public function __construct()
     {
-        add_action( 'init',  array($this, 'check_cookie') );
+        add_filter( 'redirect_canonical',  array($this, 'disable_canonical_redirect_for_front_page' ), 1);
+        add_action('init', array($this, 'add_my_rewrite'), 10, 0);
+        add_action( 'wp',  array($this, 'define_slug') );
+        add_action( 'wp',  array($this, 'set_cookie') );
         add_filter('the_content', array($this, 'check_for_frontpage'));
     }
 
-
-    public function check_cookie()  {
-        if (!isset($_GET['c']) && isset($_COOKIE['company'])) {
-            $this->slug = $_COOKIE['company'];
+    public function disable_canonical_redirect_for_front_page ($redirect)
+    {
+        if ( is_front_page() || is_home() ) {
+            $redirect = false;
         }
+        return $redirect;
+    }
 
-        if(isset($_GET['c'])){
-            setcookie("company", $_GET['c']);
-            $this->slug = $_GET['c'];
+    public function add_my_rewrite()
+    {
+        $page_on_front = get_option( 'page_on_front' );
+        add_rewrite_rule('^welcome/(.*)/?', 'index.php?index.php?page_id=' . $page_on_front .'&welcome=$matches[1]', 'top');
+        add_rewrite_tag('%welcome%','([^&]+)');
+        flush_rewrite_rules();
+    }
+
+    public function define_slug(){
+        global $wp_query;
+        if(isset($wp_query->query_vars['welcome']))
+        {
+            $this->slug= $wp_query->query_vars['welcome'];
         }
     }
-    public function check_custom_slug($content)  {
+
+    public function set_cookie()
+    {
+        if(isset($this->slug) && !isset($_COOKIE['company'])){
+            setcookie('company', $this->slug, time()+31556926, '/');
+        }
+
+        if(isset($this->slug) && isset($_COOKIE['company'])){
+            if($this->slug != $_COOKIE['company'] ) {
+                setcookie('company', $this->slug, time()+31556926, '/');
+            }
+        }
+    }
+
+    public function check_slug()
+    {
+        if($this->slug == NULL && isset($_COOKIE['company']))
+        {
+            $this->slug = $_COOKIE['company'];
+        }
+    }
+
+    public function check_for_frontpage($content)
+    {
+        if ( is_front_page() || is_home() ) {
+            $content = $this->check_custom_slug($content);
+        }
+        return $content;
+    }
+
+    public function check_custom_slug($content)
+    {
+        $this->check_slug();
+
         if($this->slug) {
             $clps = carbon_get_theme_option( 'clp-content' );
 
@@ -40,22 +88,9 @@ class Front_page {
                     break;
                 }
             }
-      } return $content;
+      }
+      return $content;
     }
-
-    public function check_for_frontpage($content) {
-        if ( is_front_page() || is_home() ) {
-            $content = $this->check_custom_slug($content);
-        }
-        return $content;
-    }
-
-    public function display_custom_message($string )  {
-        $clps = carbon_get_theme_option( 'clp-content' );
-        $string = $clps[$this->i]['champ_riche'];
-        return $string;
-    }
-
 }
 
 new Front_page();
